b834e977958e9ae30d16202f898f4efc67c7ad4f
hilizangela <talliasousa.com@gmail.com>
2023-09-14 12:21:11 -0300

    firstcommit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,21 +13,27 @@ public class Main {
 
         Agenda agendaEnderecos = new Agenda();
         System.out.println("Bem-vindo à sua agenda de endereços!\n");
+        System.out.print("Faça o cadastro do endereço: ");
         System.out.print("Digite o CEP: ");
         String cep = scanner.nextLine();
         System.out.print("Digite o Endereço: ");
         String endereco = scanner.nextLine();
         System.out.print("Digite um Ponto de Referência: ");
         String referencia = scanner.nextLine();
-
         agendaEnderecos.cadastrarEndereco(cep, endereco, referencia);
         agendaEnderecos.listarEndereco();
+        System.out.print(" Digite S para remover e N para não  ");
+        String  resposta = scanner.nextLine();
+        if(resposta.equals("s")) {
 
-        System.out.print("Digite o CEP: ");
-        String cp = scanner.nextLine();
-        agendaEnderecos.removerEndereco(cp);
+            System.out.print("Digite o CEP: ");
+            String cp = scanner.nextLine();
+            agendaEnderecos.removerEndereco(cp);
 
-        System.out.println("Programa encerrado.");
+            System.out.println("Programa encerrado.");
+        } else if(resposta.equalsIgnoreCase("n")) {
+            System.out.println("Programa encerrado.");
+        }
         scanner.close();
     }
 
